94d00cc83a3e1297cf22d2ee5b6ccc912c6c38ac
McPissPizzas <bluejw444@gmail.com>
2026-03-15 14:03:02 +0100

    Update MPP.js

--- MPP.js
+++ MPP.js
@@ -1,32 +1,34 @@
 const register = document.getElementById("Kopfleiste")
 const spalten = 5.5;
 const reihen = 5.5;
+if (window.innerWidth > 700) {
+    for (i = 0; i < 70; i++) {
+        const x = (i % spalten) * (150 / spalten)
+        const y = Math.floor(i / spalten) * (100 / reihen)
 
-for (i = 0; i < 70; i++) {
-    const x = (i % spalten) * (150 / spalten)
-    const y = Math.floor(i / spalten ) * (100 / reihen)
+        const pizza = document.createElement("div")
+        const bild = document.createElement("img")
 
-    const pizza = document.createElement("div")
-    const bild = document.createElement("img")
-
-    bild.src = "pizza_pfeil.png"
-    pizza.appendChild(bild)
-   
-
-    pizza.classList = "Pizza"
-    document.body.appendChild(pizza)
-
-    
-
-    pizza.style.left = x -20  + "vw"
-    pizza.style.top = y -100 + "vh"
+        bild.src = "pizza_pfeil.png"
+        pizza.appendChild(bild)
 
 
+        pizza.classList = "Pizza"
+        document.body.appendChild(pizza)
+
+
+
+        pizza.style.left = x - 20 + "vw"
+        pizza.style.top = y - 100 + "vh"
+
+
+    }
+    function Animpress() {
+        presses = 0
+        presses += 1
+    }
 }
-function Animpress(){
-    presses = 0
-    presses +=1
-}
+
 
 const btn = document.getElementById("babawagen")
 const audio = document.getElementById("sound")
